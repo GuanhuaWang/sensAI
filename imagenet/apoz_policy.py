d0c0f117b0a37b9cd76463f122d4b7920210a382
Brandon Hsieh <hsiehbrandon@berkeley.edu>
2019-05-04 04:50:32 +0000

    WIP: Imagenet extension, activation + pruning candidate generation is slow, estimated 51 minutes for group of size 20

---
+++ imagenet/apoz_policy.py
@@ -0,0 +1,76 @@
+import argparse
+import numpy as np
+import torch
+import io
+import glob 
+import re
+
+path = './feature_map_data/'
+file_names = [f for f in glob.glob(path + "*.pt", recursive=False)]
+group_id_list = [re.search('\(([^)]+)', f_name).group(1) for f_name in file_names]
+
+"""
+	Calculate the Average Percentage of Zeros Score of the feature map activation layer output
+"""
+def apoz_scoring(activation):
+    activation = activation.cpu()
+    if activation.dim() == 4:
+        view_2d = activation.view(-1, activation.size(2) * activation.size(3))  # (batch*channels) x (h*w)
+        featuremap_apoz = view_2d.abs().gt(0.005).sum(dim=1).float() / (activation.size(2) * activation.size(3))  # (batch*channels) x 1
+        featuremap_apoz_mat = featuremap_apoz.view(activation.size(0), activation.size(1))  # batch x channels
+    elif activation.dim() == 2:
+        featuremap_apoz_mat = activation.abs().gt(0.005).sum(dim=1).float() / activation.size(1)  # batch x 1
+    else:
+        raise ValueError("activation_channels_apoz: Unsupported shape: ".format(activation.shape))
+    return 100 - featuremap_apoz_mat.mean(dim=0).mul(100).cpu()
+
+
+def avg_scoring(activation):
+	activation = activation.cpu()
+	if activation.dim() == 4:
+		view_2d = activation.view(-1, activation.size(2) * activation.size(3))
+		featuremap_avg = view_2d.abs().sum(dim = 1).float() / (activation.size(2) * activation.size(3))
+		featuremap_avg_mat = featuremap_avg.view(activation.size(0), activation.size(1))
+	elif activation.dim() == 2:
+		featuremap_avg_mat = activation.abs().sum(dim = 1).float() / activation.size(1)
+	else:
+		raise ValueError("activation_channels_avg: Unsupported shape: ".format(activation.shape))
+	return featuremap_avg_mat.mean(dim = 0).cpu()
+
+def pruning_candidates(group_id, thresholds, file_name):
+	layers_channels = []
+	fmap_file = open(file_name, "rb")
+	data_buffer = io.BytesIO(fmap_file.read())
+	for _ in range(16):
+		layers_channels.append(torch.load(data_buffer))
+
+	candidates_by_layer = []
+	print("Calculating pruning candidates for classe(s) {}".format(group_id))
+	for index, layer in enumerate(layers_channels):
+		apoz_score = apoz_scoring(layer)
+		print(apoz_score.mean())
+
+		curr_threshold = thresholds[index]
+		while True:
+			num_candidates = apoz_score.gt(curr_threshold).sum()
+			print("Greater than {} %".format(curr_threshold), num_candidates)
+			if num_candidates < apoz_score.size()[0]:
+				candidates = [x[0] for x in apoz_score.gt(curr_threshold).nonzero().tolist()]
+				break
+			curr_threshold += 5
+
+		print("Class Index: {}, Layer {}, Number of neurons with apoz > {}%: {}/{}".format(group_id, index, curr_threshold, len(candidates), apoz_score.size()[0]))
+		candidates_by_layer.append(candidates)
+		print("Zero channels out of total in layer {}: {}/{}".format(index, len(candidates) ,len(layer)))
+	return candidates_by_layer
+
+
+if __name__ == '__main__':
+	candidates_across_classes = []
+	## Specificy threshold by layer
+	thresholds = [55] * 16
+	for group_id, file_name in zip(group_id_list, file_names):
+		group_candidates = pruning_candidates(group_id, thresholds, file_name)
+		np.save(open("prune_candidate_logs/class_({})_apoz_layer_thresholds.npy".format(group_id), "wb"), group_candidates)
+		candidates_across_classes.append(group_candidates)
+	
---
+++ imagenet/imagenet_activations.py
@@ -0,0 +1,376 @@
+import argparse
+import os
+import random
+import shutil
+import time
+import warnings
+import sys
+
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.backends.cudnn as cudnn
+import torch.distributed as dist
+import torch.optim
+import torch.multiprocessing as mp
+import torch.utils.data
+import torch.utils.data.distributed
+import torchvision.transforms as transforms
+#import torchvision.datasets as datasets
+import imagenet_dataset as datasets
+import torchvision.models as models
+
+import numpy as np
+from apoz_policy import *
+
+global num_layers
+num_layers = 0
+global layer_idx
+layer_idx = 0 
+num_batches = 0
+num_classes = 10
+
+def parse_activation(feature_map):
+    # torch.save(feature_map, feature_map_out_file)
+    global layer_idx
+    global apoz_scores_by_layer
+    global avg_scores_by_layer
+    global num_layers
+    apoz_score = apoz_scoring(feature_map).numpy()
+    avg_score = avg_scoring(feature_map).numpy()
+    
+    if apoz_scores_by_layer[layer_idx] is None:
+        apoz_scores_by_layer[layer_idx] = apoz_score
+        avg_scores_by_layer[layer_idx] = avg_score
+    else:
+        apoz_scores_by_layer[layer_idx] = np.add(apoz_scores_by_layer[layer_idx], apoz_score)
+        avg_scores_by_layer[layer_idx] = np.add(avg_scores_by_layer[layer_idx], avg_score)
+
+    layer_idx = (layer_idx + 1) % num_layers
+"""
+    Apply a hook to RelU layer
+"""
+def hook(self, input, output):
+    if self.__class__.__name__ == 'ReLU':
+       parse_activation(output.data)
+
+def apply_hook(m):
+   m.register_forward_hook(hook)
+
+
+model_names = sorted(name for name in models.__dict__
+    if name.islower() and not name.startswith("__")
+    and callable(models.__dict__[name]))
+
+parser = argparse.ArgumentParser(description='PyTorch ImageNet Training')
+parser.add_argument('data', metavar='DIR',
+                    help='path to dataset')
+parser.add_argument('-a', '--arch', metavar='ARCH', default='resnet18',
+                    choices=model_names,
+                    help='model architecture: ' +
+                        ' | '.join(model_names) +
+                        ' (default: resnet18)')
+parser.add_argument('-j', '--workers', default=4, type=int, metavar='N',
+                    help='number of data loading workers (default: 4)')
+parser.add_argument('--epochs', default=90, type=int, metavar='N',
+                    help='number of total epochs to run')
+parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
+                    help='manual epoch number (useful on restarts)')
+parser.add_argument('-b', '--batch-size', default=128, type=int,
+                    metavar='N',
+                    help='mini-batch size (default: 256), this is the total '
+                         'batch size of all GPUs on the current node when '
+                         'using Data Parallel or Distributed Data Parallel')
+parser.add_argument('--lr', '--learning-rate', default=0.1, type=float,
+                    metavar='LR', help='initial learning rate', dest='lr')
+parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
+                    help='momentum')
+parser.add_argument('--wd', '--weight-decay', default=1e-4, type=float,
+                    metavar='W', help='weight decay (default: 1e-4)',
+                    dest='weight_decay')
+parser.add_argument('-p', '--print-freq', default=1, type=int,
+                    metavar='N', help='print frequency (default: 10)')
+parser.add_argument('--resume', default='', type=str, metavar='PATH',
+                    help='path to latest checkpoint (default: none)')
+parser.add_argument('-e', '--evaluate', dest='evaluate', action='store_true',
+                    help='evaluate model on validation set')
+parser.add_argument('--pretrained', dest='pretrained', action='store_true',
+                    help='use pre-trained model')
+parser.add_argument('--world-size', default=-1, type=int,
+                    help='number of nodes for distributed training')
+parser.add_argument('--rank', default=-1, type=int,
+                    help='node rank for distributed training')
+parser.add_argument('--dist-url', default='tcp://224.66.41.62:23456', type=str,
+                    help='url used to set up distributed training')
+parser.add_argument('--dist-backend', default='nccl', type=str,
+                    help='distributed backend')
+parser.add_argument('--seed', default=None, type=int,
+                    help='seed for initializing training. ')
+parser.add_argument('--gpu', default=None, type=int,
+                    help='GPU id to use.')
+parser.add_argument('--multiprocessing-distributed', default=False, action='store_true',
+                    help='Use multi-processing distributed training to launch '
+                         'N processes per node, which has N GPUs. This is the '
+                         'fastest way to use PyTorch for either single node or '
+                         'multi node data parallel training')
+
+parser.add_argument('--group', type=int, nargs='+', default=[],
+                        help='Generate activations based on the these class indices')
+
+
+best_acc1 = 0
+
+def generate_candidates(group):
+     global num_batches
+     global num_layers
+     group_id_string = ''.join(filter(lambda x: x.isdigit() or x == '_', str(group).replace(" ", "_")))
+     apoz_thresholds = [73] * num_layers
+     avg_thresholds = [0.01] * num_layers
+     candidates_by_layer = []
+     for layer_idx, (apoz_scores, avg_scores) in enumerate(zip(apoz_scores_by_layer, avg_scores_by_layer)):
+         apoz_scores *= 1/ float(num_batches)
+         apoz_scores = torch.Tensor(apoz_scores) if not isinstance(apoz_scores, np.float64) else torch.Tensor([apoz_scores])
+
+         avg_scores *= 1/ float(num_batches)
+         avg_scores = torch.Tensor(avg_scores) if not isinstance(avg_scores, np.float64) else torch.Tensor([avg_scores])
+
+         avg_candidates = [idx for idx, score in enumerate(avg_scores) if score >= avg_thresholds[layer_idx]]
+         candidates = [x[0] for x in apoz_scores.gt(apoz_thresholds[layer_idx]).nonzero().tolist()]
+
+         difference_candidates = list(set(candidates).difference(set(avg_candidates)))
+         candidates_by_layer.append(difference_candidates)
+     print("Total candidates: {}".format(sum([len(l) for l in candidates_by_layer])))
+     np.save(open("prune_candidate_logs/class_({})_apoz_layer_thresholds.npy".format( group_id_string), "wb"), candidates_by_layer)
+     print(candidates_by_layer)
+
+
+def main():
+    args = parser.parse_args()
+
+    if args.seed is not None:
+        random.seed(args.seed)
+        torch.manual_seed(args.seed)
+        cudnn.deterministic = True
+        warnings.warn('You have chosen to seed training. '
+                      'This will turn on the CUDNN deterministic setting, '
+                      'which can slow down your training considerably! '
+                      'You may see unexpected behavior when restarting '
+                      'from checkpoints.')
+
+    if args.gpu is not None:
+        warnings.warn('You have chosen a specific GPU. This will completely '
+                      'disable data parallelism.')
+
+    main_worker(args.gpu, args)
+
+
+def main_worker(gpu, args):
+    global best_acc1
+    global num_layers
+    global apoz_scores_by_layer
+    global avg_scores_by_layer
+    args.gpu = gpu
+
+    if args.gpu is not None:
+        print("Use GPU: {} for training".format(args.gpu))
+
+    # create model
+    if args.pretrained:
+        print("=> using pre-trained model '{}'".format(args.arch))
+        model = models.__dict__[args.arch](pretrained=True)
+    else:
+        print("=> creating model '{}'".format(args.arch))
+        model = models.__dict__[args.arch]()
+
+    if args.gpu is not None:
+        torch.cuda.set_device(args.gpu)
+        model = model.cuda(args.gpu)
+     
+    # define loss function (criterion) and optimizer
+    criterion = nn.CrossEntropyLoss() # .cuda(args.gpu)
+
+    optimizer = torch.optim.SGD(model.parameters(), args.lr,
+                                momentum=args.momentum,
+                                weight_decay=args.weight_decay)
+
+    # optionally resume from a checkpoint
+    if args.resume:
+        if os.path.isfile(args.resume):
+            print("=> loading checkpoint '{}'".format(args.resume))
+            checkpoint = torch.load(args.resume)
+            args.start_epoch = checkpoint['epoch']
+            best_acc1 = checkpoint['best_acc1']
+      
+            for p in model.classifier:
+                 if isinstance(p, torch.nn.modules.activation.ReLU):
+                     num_layers += 1
+            for i, p in model.features._modules.items():
+                 if isinstance(p, torch.nn.modules.activation.ReLU):
+                     num_layers += 1
+
+            apoz_scores_by_layer = [None for _ in range(num_layers)]
+            avg_scores_by_layer = [None for _ in range(num_layers)]
+            optimizer.load_state_dict(checkpoint['optimizer'])
+            model.features = model.features.cuda(args.gpu) # cpu()
+            model = model.cuda(args.gpu) # cpu()
+            print("=> loaded checkpoint '{}' (epoch {})"
+                  .format(args.resume, checkpoint['epoch']))
+        else:
+            print("=> no checkpoint found at '{}'".format(args.resume))
+
+    # Data loading code
+    traindir = os.path.join(args.data, 'train')
+    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                     std=[0.229, 0.224, 0.225])
+
+    train_dataset = datasets.ImageFolder(
+        traindir,
+        transforms.Compose([
+            transforms.RandomResizedCrop(224),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            normalize,
+        ]), activations=True, group=args.group)
+
+    val_loader = torch.utils.data.DataLoader(
+        train_dataset, batch_size=args.batch_size, shuffle=False,
+        num_workers=args.workers, pin_memory=True, sampler=None)
+    """
+    val_loader = torch.utils.data.DataLoader(
+        datasets.ImageFolder(valdir, transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            normalize,
+        ]), group=[0], activations=True),
+        batch_size=args.batch_size, shuffle=False,
+        num_workers=args.workers, pin_memory=True)
+    """ 
+    if args.evaluate:
+        validate(val_loader, model, criterion, args)
+        generate_candidates(args.group)
+        return
+
+
+
+
+
+def validate(val_loader, model, criterion, args):
+    global layer_idx
+    global num_batches
+    batch_time = AverageMeter('Time', ':6.3f')
+    losses = AverageMeter('Loss', ':.4e')
+    top1 = AverageMeter('Acc@1', ':6.2f')
+    top5 = AverageMeter('Acc@5', ':6.2f')
+    progress = ProgressMeter(len(val_loader), batch_time, losses, top1, top5,
+                             prefix='Test: ')
+
+    # switch to evaluate mode
+    model.apply(apply_hook)
+    model.eval()
+
+    with torch.no_grad():
+        end = time.time()
+        for i, (input, target) in enumerate(val_loader):
+            num_batches += 1
+            layer_idx = 0 
+            if args.gpu is not None:
+                input = input.cuda(args.gpu) # non_blocking=True)
+            target = target.cuda(args.gpu) # non_blocking=True)
+            # compute output
+            output = model(input)
+            loss = criterion(output, target)
+
+            # measure accuracy and record loss
+            acc1, acc5 = accuracy(output, target, topk=(1, 5))
+            losses.update(loss.item(), input.size(0))
+            top1.update(acc1[0], input.size(0))
+            top5.update(acc5[0], input.size(0))
+
+            # measure elapsed time
+            batch_time.update(time.time() - end)
+            end = time.time()
+
+            if i % args.print_freq == 0:
+                progress.print(i)
+
+        # TODO: this should also be done with the ProgressMeter
+        print(' * Acc@1 {top1.avg:.3f} Acc@5 {top5.avg:.3f}'
+              .format(top1=top1, top5=top5))
+
+    return top1.avg
+
+
+def save_checkpoint(state, is_best, filename='checkpoint.pth.tar'):
+    torch.save(state, filename)
+    if is_best:
+        shutil.copyfile(filename, 'model_best.pth.tar')
+
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+    def __init__(self, name, fmt=':f'):
+        self.name = name
+        self.fmt = fmt
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+    def __str__(self):
+        fmtstr = '{name} {val' + self.fmt + '} ({avg' + self.fmt + '})'
+        return fmtstr.format(**self.__dict__)
+
+
+class ProgressMeter(object):
+    def __init__(self, num_batches, *meters, prefix=""):
+        self.batch_fmtstr = self._get_batch_fmtstr(num_batches)
+        self.meters = meters
+        self.prefix = prefix
+
+    def print(self, batch):
+        entries = [self.prefix + self.batch_fmtstr.format(batch)]
+        entries += [str(meter) for meter in self.meters]
+        print('\t'.join(entries))
+
+    def _get_batch_fmtstr(self, num_batches):
+        num_digits = len(str(num_batches // 1))
+        fmt = '{:' + str(num_digits) + 'd}'
+        return '[' + fmt + '/' + fmt.format(num_batches) + ']'
+
+
+def adjust_learning_rate(optimizer, epoch, args):
+    """Sets the learning rate to the initial LR decayed by 10 every 30 epochs"""
+    lr = args.lr * (0.1 ** (epoch // 30))
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+
+
+def accuracy(output, target, topk=(1,)):
+    """Computes the accuracy over the k top predictions for the specified values of k"""
+    with torch.no_grad():
+        maxk = max(topk)
+        batch_size = target.size(0)
+
+        _, pred = output.topk(maxk, 1, True, True)
+        pred = pred.t()
+        correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+        res = []
+        for k in topk:
+            correct_k = correct[:k].view(-1).float().sum(0, keepdim=True)
+            res.append(correct_k.mul_(100.0 / batch_size))
+        return res
+
+
+if __name__ == '__main__':
+    main()
