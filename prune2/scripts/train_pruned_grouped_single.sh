5df7cb495e834fd4de8ed79b79ee00228aab4d12
Brandon Hsieh <alexwgh333@gmail.com>
2019-11-08 02:32:01 +0000

    remove irrelavent files

--- prune2/scripts/train_pruned_grouped_single.sh
+++
@@ -1,6 +0,0 @@
-EPOCHS=160
-SAVE=./pruned2_grouped_retrain_10M_160epoch/vgg/
-i=0
-FILE=~/brandon/sensai_experiments/prune2/pruned2_models_grouped_10M/vgg/vgg_'('8_9')'_pruned_model.pth
-CUDA_VISIBLE_DEVICES=0,1,2,3 python3 cifar_group.py -a vgg --epochs ${EPOCHS} --pruned --schedule 80 120 --gamma 0.1 --resume ${FILE} --checkpoint $SAVE --train-batch 64 &
-
