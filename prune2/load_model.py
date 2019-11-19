ea0deff7cf7ce13d78c75696ee8f1f677527689c
Siyuan <suquark@gmail.com>
2019-11-18 16:57:05 -0800

    resnet arch support

--- prune2/load_model.py
+++ prune2/load_model.py
@@ -25,9 +25,20 @@ def load_pretrain_model(arch, dataset, resume_checkpoint, num_classes, use_cuda)
     # config = checkpoint['cfg']
     # model = models.__dict__[args.arch](dataset=args.dataset, depth=19, cfg=config)
     if dataset == 'cifar':
-        model = cifar_models.__dict__[arch](num_classes=num_classes)
+        if arch.startswith('vgg'):
+            model = cifar_models.__dict__[arch](num_classes=num_classes)
+        elif arch.startswith('resnet'):
+            if arch == 'resnet164':
+                depth = 164
+                block_name = 'bottleneck'
+            elif arch == 'resnet110':
+                depth = 110
+                block_name = 'bottleneck'
+            else:
+                raise NotImplementedError(f"Unsupported resnet arch: {arch}.")
+            model = cifar_models.__dict__[arch](num_classes=num_classes, depth=depth, block_name=block_name)
     else:
-        raise NotImplementedError
+        raise NotImplementedError(f"Unsupported dataaset: {dataset}.")
 
     if use_cuda:
         model.cuda()
