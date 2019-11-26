8fdd010b950470b105a92a998baf8f29572ba21c
Ubuntu <alexwgh333@gmail.com>
2019-11-26 09:02:03 +0000

    change resnet164 with real 10group classes

--- scripts/activations_grouped_resnet164_cifar100.sh
+++ scripts/activations_grouped_resnet164_cifar100.sh
@@ -1,13 +1,13 @@
 MODEL=/home/ubuntu/baseModel/pytorch-classification/checkpoints/cifar100/resnet-164/model_best.pth.tar
 rm -r prune_candidate_logs
 mkdir prune_candidate_logs
-python3 get_prune_candidates.py -a resnet164 -d cifar100 --resume $MODEL --grouped  1 2 13 32 46 51 62 77 91 93
-python3 get_prune_candidates.py -a resnet164 -d cifar100 --resume $MODEL --grouped  20 23 24 29 30 58 69 72 73 95
-python3 get_prune_candidates.py -a resnet164 -d cifar100 --resume $MODEL --grouped  33 47 49 52 56 59 66 67 76 96
-python3 get_prune_candidates.py -a resnet164 -d cifar100 --resume $MODEL --grouped  5 11 31 37 38 39 64 75 84 97
-python3 get_prune_candidates.py -a resnet164 -d cifar100 --resume $MODEL --grouped  16 21 28 41 48 81 86 87 94 99
-python3 get_prune_candidates.py -a resnet164 -d cifar100 --resume $MODEL --grouped  12 15 17 25 60 68 71 85 89 90
-python3 get_prune_candidates.py -a resnet164 -d cifar100 --resume $MODEL --grouped  3 6 19 34 35 36 43 65 80 88
-python3 get_prune_candidates.py -a resnet164 -d cifar100 --resume $MODEL --grouped  0 9 14 54 57 63 82 83 92 98
-python3 get_prune_candidates.py -a resnet164 -d cifar100 --resume $MODEL --grouped  8 10 22 26 40 50 53 61 70 79
-python3 get_prune_candidates.py -a resnet164 -d cifar100 --resume $MODEL --grouped  4 7 18 27 42 44 45 55 74 78
+python3 get_prune_candidates.py -a resnet164 -d cifar100 --resume $MODEL --grouped  0 10 53 54 57 61 62 70 83 92
+python3 get_prune_candidates.py -a resnet164 -d cifar100 --resume $MODEL --grouped  28 30 39 67 69 71 73 91 95 99
+python3 get_prune_candidates.py -a resnet164 -d cifar100 --resume $MODEL --grouped  5 9 16 20 22 25 84 86 87 94
+python3 get_prune_candidates.py -a resnet164 -d cifar100 --resume $MODEL --grouped  34 35 36 38 50 65 66 88 97 98
+python3 get_prune_candidates.py -a resnet164 -d cifar100 --resume $MODEL --grouped  6 7 14 15 19 24 40 51 75 79
+python3 get_prune_candidates.py -a resnet164 -d cifar100 --resume $MODEL --grouped  23 33 47 49 52 56 59 60 82 96
+python3 get_prune_candidates.py -a resnet164 -d cifar100 --resume $MODEL --grouped  18 26 27 29 42 44 74 77 78 93
+python3 get_prune_candidates.py -a resnet164 -d cifar100 --resume $MODEL --grouped  2 8 11 41 45 46 48 58 85 89
+python3 get_prune_candidates.py -a resnet164 -d cifar100 --resume $MODEL --grouped  3 4 21 31 43 55 63 64 72 80
+python3 get_prune_candidates.py -a resnet164 -d cifar100 --resume $MODEL --grouped  1 12 13 17 32 37 68 76 81 90
